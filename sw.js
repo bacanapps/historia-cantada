f97311eacfa4f9961fa36055dee6172083d2dd48
bacanapps <colin@cpantin.com>
2025-12-11 11:58:43 -0300

    Update cache version (v202512111201)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for História Cantada PWA
-const VERSION = "v202512111200"; // v. 11/12/2025, 12:00
+const VERSION = "v202512111201"; // v. 11/12/2025, 12:01
 const CACHE_NAME = `historia-cantada-${VERSION}`;
 
 // Files to cache for offline support
